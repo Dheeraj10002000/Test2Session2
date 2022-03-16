65bc0f3c73ed9b8dc5016650abb10b4ec10da10f
dheeraj2003 <97089373+Dheeraj10002000@users.noreply.github.com>
2022-03-16 04:52:42 +0000

    p5

--- p2final.c
+++ p2final.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("Enter three points\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  float a;
+  a=0.5*((x1*(y2-y3)) + (x2*(y3-y1)) + (x3*(y1-y2)));
+  return a;
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int a)
+{
+  if(a==0)
+    printf("it doesn't form a triangle");
+  else
+    printf("it forms a triangle");
+}
+int main()
+{
+  float x1,x2,x3,y1,y2,y3,a;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  a=is_triangle(x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,a);
+  return 0;
+}
 
